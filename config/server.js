c687267bdd2ae0c3f5a12203455dcd7c59b6afbc
karma <karam>
2022-02-07 07:19:22 +0300

    strapi first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b2e7088369ab782cc4ae4fa616e119fc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '07c6a1f6-8e6f-4505-a766-80c855fca585'
+};
